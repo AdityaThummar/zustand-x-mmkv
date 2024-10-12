e04f6ce9ca2c58f68c5b81d63322704ceb2643cf
aditya <aditya@192.168.1.4>
2024-10-12 16:37:45 +0530

    first commit

---
+++ src/ZunstandXMMKV.ts
@@ -0,0 +1,29 @@
+import { MMKV } from "react-native-mmkv";
+import { create } from "zustand";
+import { createJSONStorage, persist, PersistOptions } from "zustand/middleware";
+
+export const configureStorage = (
+  id: string = new Date().getTime().toString()
+) => {
+  const mmkv = new MMKV({ id });
+
+  return {
+    name: id,
+    storage: createJSONStorage(() => ({
+      getItem: (str: string) => mmkv.getString(str) ?? "",
+      setItem: (key: string, val: string) => mmkv.set(key, val?.toString()),
+      removeItem: (key: string) => mmkv.delete(key),
+    })),
+  } as unknown;
+};
+
+export const createPersistZustand = <T extends (arg: any) => any>(
+  name: string,
+  func: (set: (props: T) => void, get: () => T) => T
+) =>
+  create(
+    persist<T>(
+      (set: (props: T) => void, get: () => T) => func(set, get),
+      configureStorage(name) as PersistOptions<T>
+    )
+  );
